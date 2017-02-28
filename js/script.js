d82e4c8fb6581f6e0afb20c3a3164eaa37795f54
Tevin Joel Aduma <tevin74@live.com>
2017-02-28 08:11:13 +0300

    append data onto the container from the JSON file

--- js/script.js
+++ js/script.js
@@ -10,3 +10,9 @@ $.ajax({
 var to_be_run_on_server_response = function(weather_data) {
     $('#container').append(weather_data.main.temp)
 };
+
+$.ajax({
+    method: 'GET',
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=LosAngeles,us&appid=e42d32af9555a899db7106b67e9e5aae',
+    success: to_be_run_on_server_response
+});
